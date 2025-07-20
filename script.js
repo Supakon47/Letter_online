c66bf433e58b4970f5ad5cbc28b96ea6d440289e
supakon474 <supakon474@gmail.com>
2025-07-20 21:39:10 +0700

    add bg-music

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ function openLetter() {
     const emoji_heart = document.getElementById("emoji_heart");
     const emoji_bear = document.getElementById("emoji_bear");
     const emoji_cake = document.getElementById("emoji_cake");
+    const audio = document.getElementById('music');
+    const loopStart = 2;
+    const loopEnd = 19;
+
+    audio.currentTime = loopStart;
+    audio.play();
+    audio.addEventListener('timeupdate', () => {
+  if (audio.currentTime >= loopEnd) {
+    audio.currentTime = loopStart;
+    audio.play();
+  }
+});
 
     document.getElementById("top_envelope").classList.add("open");
     letter.classList.add("show");
     emoji_heart.classList.add("hide"); 
     emoji_bear.classList.add("hide");
     emoji_cake.classList.add("show");
+    audio.play();
     opened = true;
   }
 }
@@ -20,3 +33,5 @@ document.addEventListener("DOMContentLoaded", function () {
   const envelope = document.querySelector(".envelope");
   envelope.addEventListener("click", openLetter);
 });
+
+
