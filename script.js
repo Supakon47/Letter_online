05438358e45bb6db84eabc70b9adf6b11d414f9b
supakon474 <supakon474@gmail.com>
2025-07-20 19:41:44 +0700

    add emoji

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ function openLetter() {
     const letter = document.getElementById("letter");
     const emoji_heart = document.getElementById("emoji_heart");
     const emoji_bear = document.getElementById("emoji_bear");
-    
+    const emoji_cake = document.getElementById("emoji_cake");
+
     document.getElementById("top_envelope").classList.add("open");
     letter.classList.add("show");
     emoji_heart.classList.add("hide"); 
     emoji_bear.classList.add("hide");
+    emoji_cake.classList.add("show");
     opened = true;
   }
 }
